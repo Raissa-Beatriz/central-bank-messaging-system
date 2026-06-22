3c14202260d2182d7ce6c4c88bbf5805ffb711c7
Raíssa Beatriz Marinho dos Santos <20232ewbj0246@belojardim.ifpe>
2026-06-22 20:34:21 -0300

    feat(producer): adiciona produtores BankB e BankC

---
+++ producers/bank_b.py
@@ -0,0 +1,33 @@
+import time
+import pika
+from pika import PlainCredentials
+from shared.config import RABBITMQ_HOST, RABBITMQ_QUEUE, RABBITMQ_USER, RABBITMQ_PASSWORD
+from shared.utils import generate_transaction, transaction_to_json
+
+BANK_NAME = "BankB"
+TARGET_BANK = "BankC"
+
+def main():
+    credentials = PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=RABBITMQ_HOST, credentials=credentials)
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue=RABBITMQ_QUEUE, durable=True)
+
+    while True:
+        transaction = generate_transaction(BANK_NAME, TARGET_BANK)
+        message = transaction_to_json(transaction)
+
+        channel.basic_publish(
+            exchange="",
+            routing_key=RABBITMQ_QUEUE,
+            body=message,
+            properties=pika.BasicProperties(delivery_mode=2)
+        )
+
+        print(f"[{BANK_NAME}] Enviou: {message}")
+        time.sleep(3)
+
+if __name__ == "__main__":
+    main()
